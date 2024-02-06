70dadf8a61fd1745a41713cc92259b0d4df0898d
reginaisabellla <re231627@ucf.edu>
2024-02-05 22:28:13 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dig4639-ex3
+"The way to get started is to quit talking and begin doing. -Walt Disney"
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+    "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller",
+    "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that. -Martin Luther King Jr."
+]
+
+console.log(quotes[Math.floor(Math.random()*3)])
